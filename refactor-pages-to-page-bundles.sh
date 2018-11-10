9f31fc602bc21fa7f9df33279796e5d1a84ee2d6
Nathan Genetzky <nathan@genetzky.us>
2018-11-10 16:03:58 -0600

    refactor-pages-to-page-bundles: Switch from bash to sh

--- refactor-pages-to-page-bundles.sh
+++ refactor-pages-to-page-bundles.sh
@@ -1,25 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
 SCRIPTDIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"
 
 refactor_pages_to_page_bundles()
 {
-  cd "${SCRIPTDIR}/../"
+  if [ ! -d ./content/ ]; then
+    echo "Please run script from root of hugo site"
+  fi
   local files="$(find ./content/ -iname '*.md' -not -iname '*index.md' -not -ipath './content/home/*')"
   for file in ${files}; do
     local pagedir="${file%.md}"
 
     echo "${file} -> ${pagedir}/index.md"
-    [[ -d "${pagedir}" ]] || mkdir "${pagedir}"
+    if [ -d "${pagedir}" ]; then
+      mkdir "${pagedir}"
+    fi
 
     mv --no-target-directory "${file}" "${pagedir}/index.md"
   done
 }
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    # Bash Strict Mode
-    set -eu -o pipefail
+# Bash Strict Mode
+set -eu
 
-    # set -x
-    refactor_pages_to_page_bundles "$@"
-fi
+# set -x
+refactor_pages_to_page_bundles "$@"
